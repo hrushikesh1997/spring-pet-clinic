1ef6d5d9105301752cd75cb80a761ae1cef2e37b
Hrushikesh Mahapatro <hmahapat@cisco.com>
2021-01-24 00:39:57 +0530

    POJOs Added

---
+++ src/main/java/com/example/springpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package com.example.springpetclinic.model;
+
+public class PetType {
+	private String petName;
+
+	public String getPetName() {
+		return petName;
+	}
+
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
+}
